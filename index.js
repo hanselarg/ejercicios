69c4b51e8ae24a87e810c031dc1e8068934b2b04
Hans Thiel <hsthiel@gmail.com>
2019-03-07 01:03:05 -0300

    https://github.com/hanselarg/ejercicios.git

---
+++ index.js
@@ -0,0 +1,43 @@
+// para que una variable tenga valor
+// tenes que poner el breakpoint en la linea siguiente a donde queres examinar
+// 1- declaracion y asignacion de variables
+/* console.log("Esto es una prueba")
+let a = 10; 
+let b = 20;
+let suma = a + b;
+console.log(suma); */
+
+// 2- funciones
+//declaracion de la funcion
+/* function suma(a,b){  
+   return a + b;
+ }
+//ejecutando la funcion
+let unaSuma = suma(10,20);
+
+let otraSuma = suma(30,40);
+
+console.log("funciona suma", unaSuma);
+
+console.log("funciona otraSuma", otraSuma); */
+
+function suma(a,b){  
+    return a + b;
+}
+
+/* $(document).ready(function(){
+
+    $.ajax({
+        dataType: "json",
+        url: url,
+        data: data,
+        success: success
+      });
+
+    $("#contenido").html("hola");
+}); */
+
+
+
+
+
